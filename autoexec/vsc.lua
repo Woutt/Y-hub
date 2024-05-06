908b41dd6706ddf395db2a781fc3f1fbd4f21fd4
Woutt <71903277+Woutt@users.noreply.github.com>
2024-05-06 18:31:44 +0200

    Add files via upload

---
+++ autoexec/vsc.lua
@@ -0,0 +1,22 @@
+local WebSocket = assert(WebSocket or Websocket or websocket)
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+while true do
+    local success, client = pcall(WebSocket.connect, "ws://localhost:33882/")
+    if success then
+        client.OnMessage:Connect(function(payload)
+            local callback, exception = loadstring(payload)
+            if exception then
+                error(exception, 2)
+            end
+
+            task.spawn(callback)
+        end)
+
+        client.OnClose:Wait()
+    end
+
+    task.wait(1)
+end
